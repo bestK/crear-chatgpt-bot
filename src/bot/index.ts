ec6667effdf85b22ab10c8178eafab9d0805dd34
ECH2O <1138493417@qq.com>
2023-10-24 03:14:13 +0000

    feat:support stream

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -78,24 +78,79 @@ export const createBot = (
 
         if (ctx.chat?.type === 'private' && text) {
             await ctx.replyWithChatAction('typing');
+            const chat_id = ctx.chat.id;
+            const prev_message = '⌛';
+            const prev_reply = await ctx.api.sendMessage(chat_id, prev_message);
+            const reply_id = prev_reply.message_id;
 
-            const completion = await openai.createChatCompletion(
-                {
-                    model: 'gpt-3.5-turbo',
-                    max_tokens: 4000,
-                    messages: [{ role: 'user', content: text }]
-                },
-                { timeout: 20000 }
-            );
+            try {
+                const res: any = await openai.createChatCompletion(
+                    {
+                        model: 'gpt-3.5-turbo',
+                        max_tokens: 4000,
+                        messages: [{ role: 'user', content: text }],
+                        stream: true
+                    },
+                    { responseType: 'stream' }
+                );
 
-            const response = completion.data.choices.at(0)?.message;
-            if (response) {
-                await ctx.reply(response.content);
+                let complete_message = '';
+                let edit_message_size = 0;
+
+                res.data.on('data', async (data: { toString: () => string }) => {
+                    const lines = data
+                        .toString()
+                        .split('\n')
+                        .filter((line: string) => line.trim() !== '');
+
+                    const promises = [];
+
+                    for (const line of lines) {
+                        const message = line.replace(/^data: /, '');
+                        if (message === '[DONE]') {
+                            return; // Stream finished
+                        }
+                        try {
+                            const parsed = JSON.parse(message);
+                            const chunk = parsed?.choices[0]?.delta?.content;
+                            if (chunk) {
+                                complete_message = `${complete_message}${chunk}`;
+                                const complete_message_size = complete_message.length;
+                                if (complete_message_size < 10 || complete_message_size - edit_message_size > 10) {
+                                    edit_message_size = complete_message.length;
+                                    promises.push(ctx.api.editMessageText(chat_id, reply_id, complete_message));
+                                }
+                            }
+                        } catch (error) {
+                            console.error('Could not JSON parse stream message', message, error);
+                        }
+                    }
+
+                    await Promise.all(promises);
+                });
+            } catch (error: any) {
+                if (error.response?.status) {
+                    console.error(error.response.status, error.message);
+                    error.response.data.on('data', (data: { toString: () => any }) => {
+                        const message = data.toString();
+                        try {
+                            const parsed = JSON.parse(message);
+                            console.error('An error occurred during OpenAI request: ', parsed);
+                        } catch (error) {
+                            console.error('An error occurred during OpenAI request: ', message);
+                        }
+                    });
+                } else {
+                    console.error('An error occurred during OpenAI request', error);
+                }
             }
         }
     });
 
     return bot;
 };
+function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 export type Bot = ReturnType<typeof createBot>;
